b3ad21a9d606faff6c8a18e817eac6488be653a7
eduardosoutodefreitas <dev.edufreitas@gmail.com>
2024-01-26 10:38:28 -0300

    feat: add initialTasks constant

---
+++ constants/initialTasks.ts
@@ -0,0 +1,24 @@
+import { Task } from '@/types/Task';
+
+export const initialTasks: Task[] = [
+  {
+    id: 'f88e94ca-9890-437f-a02f-669be43f305b',
+    description: 'Lavar as mãos',
+    completed: false,
+  },
+  {
+    id: 'fdc6f22e-4ed5-4a1f-bdb2-ffacf8af5a03',
+    description: 'Fazer um bolo',
+    completed: false,
+  },
+  {
+    id: 'e9db7c03-3239-4cbf-9396-a811047e54a6',
+    description: 'Lavar a louça',
+    completed: false,
+  },
+  {
+    id: '3c9b3416-1d4b-4cf7-9d4c-501f5b6d0814',
+    description: 'Levar o lixo para fora',
+    completed: true,
+  },
+];
